a3b5c9920aac69fa8db393eb61e0de6892c08917
Lucas Leblow <leblowl@gmail.com>
2015-07-30 16:12:31 -0700

    removed setup.py for testing

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name = 'tile-gen',
-      version = '0.1.0',
-      classifiers = ['Programming Language :: Python :: 2.7'],
-      packages = ['tile_gen', 'tile_gen.vectiles'],
-      package_dir = {'tile_gen': 'src/tile_gen'})
