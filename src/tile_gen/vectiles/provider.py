fe6875f2510c1056e97fb2f42ad86320f0ab4ef3
Lucas Leblow <leblowl@gmail.com>
2015-08-24 10:32:50 -0700

    global keyword added to provider init

--- src/tile_gen/vectiles/provider.py
+++ src/tile_gen/vectiles/provider.py
@@ -21,6 +21,7 @@ tolerances = [6378137 * 2 * pi / (2 ** (zoom + 8)) for zoom in range(22)]
 db = None
 
 def init(dbinfo):
+    global db
     conn = connect(**dbinfo)
     conn.set_session(readonly=True, autocommit=True)
     db = conn.cursor(cursor_factory=RealDictCursor)
